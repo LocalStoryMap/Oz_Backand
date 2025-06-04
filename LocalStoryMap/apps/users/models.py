6e43733da2f1d8e7c371a281b275fded021eb0e9
JunHo-L <wnsgh4368@naver.com>
2025-06-04 16:41:56 +0900

    fix:모델수정

--- LocalStoryMap/apps/users/models.py
+++ LocalStoryMap/apps/users/models.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -7,9 +9,6 @@ class User(AbstractUser):
     provider = models.CharField(max_length=30)
     social_id = models.CharField(max_length=100, unique=True)
     profile_image = models.URLField(max_length=500, blank=True, null=True)
-
-    first_name = models.CharField(max_length=150, blank=True, null=True)
-    last_name = models.CharField(max_length=150, blank=True, null=True)
 
     is_paid_user = models.BooleanField(default=False)  # 인앱결제 유저 여부
     is_staff = models.BooleanField(default=False)  # 관리자 여부
