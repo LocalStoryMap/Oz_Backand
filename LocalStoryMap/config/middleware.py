3cbd00dc2ee794ade76757b86acad796c7a2448d
Alex424525 <sht914@gmail.com>
2025-06-13 14:42:14 +0900

    black,isort,mypy검사통과

--- LocalStoryMap/config/middleware.py
+++ LocalStoryMap/config/middleware.py
@@ -1,5 +1,6 @@
 from sentry_sdk import capture_exception
 
+
 class LogAllErrorsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -8,6 +9,8 @@ class LogAllErrorsMiddleware:
         response = self.get_response(request)
 
         if response.status_code in [400, 403, 404, 405]:
-            capture_exception(Exception(f"{response.status_code} Error on path: {request.path}"))
+            capture_exception(
+                Exception(f"{response.status_code} Error on path: {request.path}")
+            )
 
         return response
