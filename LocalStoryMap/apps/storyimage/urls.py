1e2324acd3a11b162e6ab0b52c45cedf09e3f4a6
SWWS97 <parkmon2@naver.com>
2025-06-18 17:46:26 +0900

    storyimage 로직 수정

--- LocalStoryMap/apps/storyimage/urls.py
+++ LocalStoryMap/apps/storyimage/urls.py
@@ -1,21 +1,15 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from rest_framework_nested import routers
-
+from django.urls import path
 from .views import StoryImageViewSet
 
-# 기본 이미지 CRUD
-router = DefaultRouter()
-# router.register(r'stories', StoryViewSet, basename='story')
-router.register(r"images", StoryImageViewSet, basename="image")
-# → /api/images/, /api/images/{pk}/
-
-# stories/{pk}/images
-# nested = routers.NestedDefaultRouter(router, r'stories', lookup='story')
-# nested.register(r'images', StoryImageViewSet, basename='story-images')
-# → /api/stories/{story_pk}/images/, /api/stories/{story_pk}/images/{pk}/
+story_image_list = StoryImageViewSet.as_view({
+    "get": "list",
+    "post": "create"
+})
+story_image_detail = StoryImageViewSet.as_view({
+    "delete": "destroy"
+})
 
 urlpatterns = [
-    path("", include(router.urls)),  # /images, /images/{pk}
-    # path("", include(nested.urls)),    # /stories/{pk}/images, /stories/{pk}/images/{pk}
+    path("stories/<str:story_id>/images/", story_image_list, name="story-image-list"),
+    path("stories/<str:story_id>/images/<int:pk>/", story_image_detail, name="story-image-detail"),
 ]
