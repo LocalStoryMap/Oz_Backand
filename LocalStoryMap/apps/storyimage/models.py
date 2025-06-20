c2b7c4918774895d51e459838df753558e210fb6
Alex424525 <sht914@gmail.com>
2025-06-20 13:00:55 +0900

    fix:models.py수정

--- LocalStoryMap/apps/storyimage/models.py
+++ LocalStoryMap/apps/storyimage/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from apps.story.models import Story
 from apps.users.models import User
 
+from storages.backends.s3boto3 import S3Boto3Storage
+
+s3_storage = S3Boto3Storage()
+
 
 class StoryImage(models.Model):
     image_id = models.BigAutoField(primary_key=True)
@@ -11,7 +15,7 @@ class StoryImage(models.Model):
     story = models.ForeignKey(
         Story, on_delete=models.CASCADE, related_name="storyimages"
     )
-    image_file = models.ImageField(upload_to="story_images/%Y/%m/%d/")
+    image_file = models.ImageField(upload_to="story_images/%Y/%m/%d/", storage=s3_storage,)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
