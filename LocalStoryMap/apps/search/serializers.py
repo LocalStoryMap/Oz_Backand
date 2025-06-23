7de0a48722eba250f0a1636f5a967345886523c6
Alex424525 <sht914@gmail.com>
2025-06-23 15:35:27 +0900

    fix:search/serializers.py & views.py코드수정#1

--- LocalStoryMap/apps/search/serializers.py
+++ LocalStoryMap/apps/search/serializers.py
@@ -1,18 +1,22 @@
 from rest_framework import serializers
 
-from apps.marker.models import Marker  # 예시용
+from apps.marker.models import Marker
 from apps.story.models import Story
 from apps.users.models import User
 
 
 class StorySearchResultSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(source="story_id", read_only=True)
+    title = serializers.CharField(source="story", read_only=True)
+    content = serializers.CharField(source="summary_text", read_only=True)
+
     class Meta:
         model = Story
         fields = ["id", "title", "content"]
 
 
 class UserSearchResultSerializer(serializers.ModelSerializer):
-    stories = StorySearchResultSerializer(many=True, source="stories")
+    stories = StorySearchResultSerializer(many=True)
 
     class Meta:
         model = User
@@ -25,6 +29,10 @@ class UserSearchResultSerializer(serializers.ModelSerializer):
 
 
 class MarkerSearchResultSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(source="marker_name", read_only=True)
+    description = serializers.CharField(read_only=True)
+
     class Meta:
         model = Marker
         fields = ["id", "title", "description"]
