269522c4b57118a267a4f8dbe21271723314c8f1
SWWS97 <parkmon2@naver.com>
2025-06-26 10:12:07 +0900

    bookmark 수정

--- LocalStoryMap/apps/bookmark/urls.py
+++ LocalStoryMap/apps/bookmark/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import BookmarkViewSet
 
 router = DefaultRouter()
-router.register(r"bookmarks", BookmarkViewSet, basename="bookmark")
+router.register(r"bookmarks", BookmarkViewSet, basename="bookmarks")
 
 urlpatterns = router.urls
--- LocalStoryMap/apps/bookmark/views.py
+++ LocalStoryMap/apps/bookmark/views.py
@@ -1,68 +1,67 @@
-from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
+from rest_framework import mixins, viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import mixins, permissions, viewsets
 
-from .models import Bookmark
+from .models import Bookmark, Story
 from .serializers import BookmarkSerializer
 
-
-@method_decorator(
-    name="list",
-    decorator=swagger_auto_schema(
-        tags=["북마크"],
-        operation_summary="스토리 북마크 목록 조회",
-        responses={
-            200: openapi.Response(
-                description="북마크 불러오기 성공.", schema=BookmarkSerializer(many=True)
-            )
-        },
-    ),
-)
-@method_decorator(
-    name="create",
-    decorator=swagger_auto_schema(
-        tags=["북마크"],
-        operation_summary="스토리 북마크 추가",
-        responses={
-            201: openapi.Response(
-                description="북마크 추가됨.", schema=BookmarkSerializer()  # 단일 객체 응답
-            )
-        },
-    ),
-)
-@method_decorator(
-    name="destroy",
-    decorator=swagger_auto_schema(
-        tags=["북마크"],
-        operation_summary="스토리 북마크 삭제",
-        responses={204: openapi.Response(description="북마크 삭제됨.", schema=None)},
-    ),
-)
 class BookmarkViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
-):
+    mixins.ListModelMixin,
+                      ):
     """
     list:
-    Return a list of the current user's bookmarks.
+        북마크 목록 조회 (GET /bookmarks/)
 
-    create:
-    Bookmark a story for the current user.
+    add:
+        스토리 PK로 북마크 추가 (POST /bookmarks/{story_id}/)
 
-    destroy:
-    Remove a bookmark by its ID.
+    remove:
+        스토리 PK로 북마크 삭제 (DELETE /bookmarks/{story_id}/)
     """
-
-    serializer_class = BookmarkSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    serializer_class    = BookmarkSerializer
+    permission_classes  = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Bookmark.objects.filter(user=self.request.user)
 
-    def perform_create(self, serializer):
-        # nested URL에서 story_pk를 꺼내와 바인딩
-        story_id = self.kwargs.get("story_pk")
-        serializer.save(user=self.request.user, story_id=story_id)
+    @swagger_auto_schema(
+        tags=["북마크"],
+        operation_summary="스토리 북마크 추가",
+        responses={
+            201: openapi.Response("북마크 추가됨",    schema=BookmarkSerializer()),
+            200: openapi.Response("이미 북마크가 존재함", schema=BookmarkSerializer()),
+        },
+    )
+    @action(detail=True, methods=["post"], url_path="", url_name="add")
+    def add(self, request, pk=None):
+        """POST /bookmarks/{pk}/  → story_id=pk"""
+        story, _ = get_object_or_404(Story, pk=pk), None
+        bookmark, created = Bookmark.objects.get_or_create(
+            user=request.user,
+            story=story
+        )
+        serializer = self.get_serializer(bookmark)
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_201_CREATED if created else status.HTTP_200_OK
+        )
+
+    @swagger_auto_schema(
+        tags=["북마크"],
+        operation_summary="스토리 북마크 삭제",
+        responses={204: openapi.Response("북마크 삭제됨", schema=None)},
+    )
+    @action(detail=True, methods=["delete"], url_path="", url_name="remove")
+    def remove(self, request, pk=None):
+        """DELETE /bookmarks/{pk}/  → story_id=pk"""
+        bookmark = get_object_or_404(
+            Bookmark,
+            user=request.user,
+            story_id=pk
+        )
+        bookmark.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
